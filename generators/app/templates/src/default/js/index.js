542903faa71a4aaf9ae2a303d2f7342646ce3208
fly <fly@SHHQM0008.local>
2017-09-15 17:52:10 +0800

    *

--- generators/app/templates/src/default/js/index.js
+++ generators/app/templates/src/default/js/index.js
@@ -1,19 +1,2 @@
 import '../css/style.scss';
-var WX = require('./wx-config.js');
-
-WX.init(function () {
-  WX.share({
-    title: "",
-    desc: "",
-    link: window.location.href,
-    imgUrl: "https://source.vipabc.com/ext/images/tutorabc-app/tutorabc_220X220.png"
-  });
-});
-WX.share({
-  title: "",
-  desc: "",
-  link: window.location.href,
-  imgUrl: "https://source.vipabc.com/ext/images/tutorabc-app/tutorabc_220X220.png"
-});
-WX.pay('','1');
 
