9c330ffc32e3dbd135dfdffa9344d87a2b69a510
fly <fly@default.com>
2018-07-02 16:49:22 +0800

    update

--- generators/app/templates/build/webpack.common.js
+++ generators/app/templates/build/webpack.common.js
@@ -36,12 +36,28 @@ module.exports = {
         })
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"]
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'assets/img/[name].[hash:7].[ext]'
+        }
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"]
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'assets/media/[name].[hash:7].[ext]'
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'assets/fonts/[name].[hash:7].[ext]'
+        }
       },
       {
         test: /\.(csv|tsv)$/,
