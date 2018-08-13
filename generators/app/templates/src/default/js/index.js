c1dbab21b454b1c31d90c6fa7330c9eb70476622
fly <fly@default.com>
2018-08-13 14:04:48 +0800

    update

--- generators/app/templates/src/default/js/index.js
+++ generators/app/templates/src/default/js/index.js
@@ -2,33 +2,6 @@ import 'normalize.css';
 import '../css/style.scss';
 import wxsdk from "./wxsdk.js";
 
-if (process.env.NODE_ENV == "uat") {
-  function loadJS(src, callback) {
-    var script = document.createElement("script");
-    var head = document.getElementsByTagName("head")[0];
-    var loaded;
-    script.src = src;
-    if (typeof callback === "function") {
-      script.onload = script.onreadystatechange = function () {
-        if (
-          !loaded &&
-          (!script.readyState || /loaded|complete/.test(script.readyState))
-        ) {
-          script.onload = script.onreadystatechange = null;
-          loaded = true;
-          callback();
-        }
-      };
-    }
-    head.appendChild(script);
-  }
-  loadJS('https://finfinity.github.io/source/js/vconsole.min.js', function () {
-    var vConsole = new VConsole();
-  });
-}
-
-console.log("hello world");
-
 wxsdk.init({
   api: '/',
   title: 'FLY',
