d36bf8739c9f1c040b78abbe9208a031a698e79c
fly <fly@default.com>
2018-08-13 14:21:19 +0800

    update

--- generators/app/templates/build/webpack.prod.js
+++ generators/app/templates/build/webpack.prod.js
@@ -2,20 +2,12 @@ const webpack = require('webpack');
 const path = require("path");
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   devtool: 'cheap-module-source-map',
   plugins: [
     new UglifyJSPlugin(),
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, '../static'),
-        to: path.resolve(__dirname, "../dist/static"),
-        ignore: ['.*']
-      }
-    ]),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
