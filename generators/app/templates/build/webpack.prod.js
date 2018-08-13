5bb3304394b277ba07c81a9c5a0dd44918b3232b
fly <fly@default.com>
2018-08-13 14:22:36 +0800

    update

--- generators/app/templates/build/webpack.prod.js
+++ generators/app/templates/build/webpack.prod.js
@@ -1,5 +1,4 @@
 const webpack = require('webpack');
-const path = require("path");
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
